7f180f5cc8f32e5cd8b4d1029da986382c980712
Tomas Tomecek <tomas.tomecek@hotmail.com>
2021-02-26 10:51:15 +0100

    Use form and submit event instead of multiple click events and anchor tags #1

--- assets/javascripts/dialog.js
+++ assets/javascripts/dialog.js
@@ -1,3 +1,6 @@
+const rResponse = document.getElementById("r-response");
+const submitForm = document.getElementById('submit-form');
+
 const delay = (function () {
   let timer = 0;
   return function (callback, ms) {
@@ -6,41 +9,41 @@ const delay = (function () {
   };
 })();
 
-function sendRequest(vote) {
-  chrome.tabs.getSelected(null, async function (tab) {
-    const data = {
-      vote,
-      url: tab.url,
-      title: tab.title,
-    };
-    const url = 'https://stg.postfactum.sk/api/v1/source_articles/submit/';
-    // Keep both URLs for fast switching between local and remote servers
-    // const url = 'http://127.0.0.1:8000/api/v1/source_articles/submit/';
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-    const responseDiv = document.getElementById("r-response");
-    responseDiv.classList.add("r-response");
-    responseDiv.innerHTML = "Požiadavka bola odoslaná.<br>Ďakujeme";
-    delay(function () {
-      responseDiv.innerHTML = "";
-      responseDiv.classList.remove("r-response");
-    }, 2000);
-  })
+async function sendRequest(vote) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.tabs.getSelected(null, async (tab) => {
+        const data = {
+          vote,
+          url: tab.url,
+          title: tab.title,
+        };
+        const url = 'https://stg.postfactum.sk/api/v1/source_articles/submit/';
+        // Keep both URLs for fast switching between local and remote servers
+        // const url = 'http://127.0.0.1:8000/api/v1/source_articles/submit/';
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+        return resolve(response);
+      });
+    } catch (error) {
+      return reject(error)
+    }
+  });
 }
 
-document.getElementById('submit-page').addEventListener('click', function () {
-  sendRequest("submit_url")
-});
-
-document.getElementById('up-vote').addEventListener('click', function () {
-  sendRequest("upvote_url")
-});
-
-document.getElementById('down-vote').addEventListener('click', function () {
-  sendRequest("downvote_url")
+submitForm.addEventListener('submit', async function (event) {
+  event.preventDefault();
+  const vote = event.submitter.name;
+  await sendRequest(vote);
+  rResponse.classList.add("r-response");
+  rResponse.innerHTML = "Požiadavka bola odoslaná.<br>Ďakujeme";
+  delay(() => {
+    rResponse.innerHTML = "";
+    rResponse.classList.remove("r-response");
+  }, 2000);
 });
