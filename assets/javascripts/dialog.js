5381c2fde07c254a3181f9f4699b84235bf344ba
Tomas Tomecek <tomas.tomecek@hotmail.com>
2021-03-15 01:40:00 +0100

    Add integration test user upvotes from popup

--- assets/javascripts/dialog.js
+++ assets/javascripts/dialog.js
@@ -1,6 +1,3 @@
-const rResponse = document.getElementById("r-response");
-const submitForm = document.getElementById('submit-form');
-
 const delay = (function () {
   let timer = 0;
   return function (callback, ms) {
@@ -9,14 +6,20 @@ const delay = (function () {
   };
 })();
 
-submitForm.addEventListener('submit', async function (event) {
-  event.preventDefault();
-  const vote = event.submitter.name;
-  await PostFactum.sendRequest(vote);
-  rResponse.classList.add("r-response");
-  rResponse.innerHTML = "Požiadavka bola odoslaná.<br>Ďakujeme";
-  delay(() => {
-    rResponse.innerHTML = "";
-    rResponse.classList.remove("r-response");
-  }, 2000);
+document.addEventListener('DOMContentLoaded', async function (event) {
+  const rResponse = document.getElementById("r-response");
+  const submitForm = document.getElementById('submit-form');
+  await PostFactum.init(getConfig);
+
+  submitForm.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const vote = event.submitter.name;
+    await PostFactum.sendRequest(vote);
+    rResponse.classList.add("r-response");
+    rResponse.innerHTML = "Požiadavka bola odoslaná.<br>Ďakujeme";
+    delay(() => {
+      rResponse.innerHTML = "";
+      rResponse.classList.remove("r-response");
+    }, 2000);
+  });
 });
---
+++ test/integration/user-upvotes-page.test.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const puppeteer = require('puppeteer');
+const assert = require("assert");
+const extensionPath = path.resolve();
+
+async function bootBrowser() {
+  return puppeteer.launch({
+    headless: false,
+    args: [
+      `--disable-extensions-except=${extensionPath}`,
+      `--load-extension=${extensionPath}`,
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
+}
+
+describe('User upvotes page', function () {
+  let browser;
+  let page;
+  this.timeout(20000); // default is 2 seconds and that may not be enough to boot browsers and pages.
+
+  before(async () => {
+    browser = await bootBrowser();
+  });
+
+  after(async () => {
+    await browser.close();
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto('chrome://extensions');
+    const extensionId = await page.evaluate(async () => {
+      const [ext] = await chrome.management.getAll();
+      return ext.id;
+    });
+    await page.goto(`chrome-extension://${extensionId}/dialog.html`);
+  });
+
+  afterEach(async () => {
+    await page.close();
+  });
+
+  context('when popup.html', () => {
+    it('clicks upvote button', async () => {
+      await page.setRequestInterception(true)
+      page.on('request', (request) => {
+        if (request.url() === 'http://localhost:8000/api/v1/source_articles/submit/') {
+          if (request.method() === 'POST') {
+            request.respond({
+              status: 200,
+              headers: {
+                'access-control-allow-origin': '*',
+              },
+              content: 'application/json',
+              body: JSON.stringify({ ok: true }),
+            });
+          } else if (request.method() === 'OPTIONS') {
+            request.respond({
+              status: 204,
+              headers: {
+                'access-control-allow-headers': 'content-type',
+                'access-control-allow-methods': 'GET,POST,PUT,DELETE,OPTIONS,HEAD',
+                'access-control-allow-origin': '*',
+              },
+            });
+          }
+        } else {
+          request.continue();
+        }
+      });
+
+      await page.click('button[name="upvote"]');
+      await page.waitForSelector('.r-response');
+      const responseText = await page.$eval('#r-response', response => response.innerText);
+      assert.strictEqual(responseText, 'POŽIADAVKA BOLA ODOSLANÁ.\nĎAKUJEME');
+    });
+  });
+});
