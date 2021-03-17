29f5f6464c2b784a8721ec9d4d80cc2ebf8acf34
Tomas Tomecek <tomas.tomecek@hotmail.com>
2021-03-17 20:13:58 +0100

    Do not set menu items async

--- src/background.js
+++ src/background.js
@@ -5,23 +5,23 @@ importScripts(
 
 (async () => {
   await PostFactum.init(getConfig);
-
-  const upvoteMenuItem = {
-    id: 'upvote-link',
-    title: 'Dôveryhodná stránka',
-  };
-  const downvoteMenuItem = {
-    id: 'downvote-link',
-    title: 'Nedôveryhodná stránka',
-  };
-
-  chrome.contextMenus.create(upvoteMenuItem);
-  chrome.contextMenus.create(downvoteMenuItem);
-  chrome.contextMenus.onClicked.addListener(async (event) => {
-    if (event.menuItemId === 'upvote-link') {
-      await PostFactum.sendRequest('upvote');
-    } else if (event.menuItemId === 'downvote-link') {
-      await PostFactum.sendRequest('downvote');
-    }
-  });
 })();
+
+const upvoteMenuItem = {
+  id: 'upvote-link',
+  title: 'Dôveryhodná stránka',
+};
+const downvoteMenuItem = {
+  id: 'downvote-link',
+  title: 'Nedôveryhodná stránka',
+};
+
+chrome.contextMenus.create(upvoteMenuItem);
+chrome.contextMenus.create(downvoteMenuItem);
+chrome.contextMenus.onClicked.addListener((event) => {
+  if (event.menuItemId === 'upvote-link') {
+    return PostFactum.sendRequest('upvote');
+  } else if (event.menuItemId === 'downvote-link') {
+    return PostFactum.sendRequest('downvote');
+  }
+});
