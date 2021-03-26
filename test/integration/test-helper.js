70ccccbff951b20230dbf6b413e11325983363db
Tomas Tomecek <tomas.tomecek@hotmail.com>
2021-03-26 22:56:52 +0100

    Add integration tests for response errors

--- test/integration/test-helper.js
+++ test/integration/test-helper.js
@@ -1,3 +1,9 @@
+function randomFrom(collection) {
+  const randomIndex = Math.floor(Math.random() * collection.length);
+  const statusCode = collection[randomIndex];
+  return statusCode;
+}
+
 const getExtensionId = async (page) => {
   await page.goto('chrome://extensions');
   const extensionId = await page.evaluate(async () => {
@@ -9,4 +15,5 @@ const getExtensionId = async (page) => {
 
 module.exports = {
   getExtensionId,
+  randomFrom,
 };
